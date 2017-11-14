0e1d5b5b896d16d8974aafa61cb8387861906642
Aditya Earanky <adityahero.evn@gmail.com>
2017-11-14 10:30:51 -0500

    commit #4 : "displaying a list"

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,24 +1,25 @@
-import {Component, OnInit, ViewEncapsulation} from '@angular/core';
-import {Hero} from '../hero';
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
-    selector: 'app-heroes', // components css element selector
-    templateUrl: './heroes.component.html', // location of components template file
-    styleUrls: ['./heroes.component.css'], // location of components private CSS styles
-    encapsulation: ViewEncapsulation.None
+    selector: 'app-heroes',
+    templateUrl: './heroes.component.html',
+    styleUrls: ['./heroes.component.css']
 })
+export class HeroesComponent implements OnInit {
 
-export class HeroesComponent implements OnInit { // always export this class
+    heroes = HEROES;
 
-    hero: Hero = {
-        id: 1,
-        name: 'Windstorm'
-    };
+    selectedHero: Hero;
 
-    constructor() {
+
+    constructor() { }
+
+    ngOnInit() {
     }
 
-    ngOnInit() { // good place to put initialization logic
+    onSelect(hero: Hero): void {
+        this.selectedHero = hero;
     }
-
 }
